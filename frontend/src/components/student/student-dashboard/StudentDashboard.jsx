ba72aad685f80e19615a843556b446f069ab9943
Tiela Black-Law <tiela.rose@gmail.com>
2023-06-19 17:32:37 -0700

    Begin to build out StudentDashboard

--- frontend/src/components/student/student-dashboard/StudentDashboard.jsx
+++ frontend/src/components/student/student-dashboard/StudentDashboard.jsx
@@ -1,3 +1,22 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+
 export default function StudentDashboard() {
-  return <p>this is the student dashboard</p>;
+  const allContext = useContext(AppContext);
+  const currentStudent = allContext.currentUser;
+
+  return (
+    <>
+      <h2>
+        Welcome, {currentStudent.student_first_name}{" "}
+        {currentStudent.student_last_name}!
+      </h2>
+
+      <p>You are working on Goal {currentStudent.current_problem_set}</p>
+
+      <p>Ready to practice?</p>
+
+      <button>Let's Begin!</button>
+    </>
+  );
 }
