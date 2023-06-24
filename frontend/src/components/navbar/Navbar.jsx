2e1311e62b27b610152e198eab8c253a6421ac2f
Tiela Black-Law <tiela.rose@gmail.com>
2023-06-24 13:20:13 -0700

    Update Navbar to include link to student dashboard, username for all users

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -7,7 +7,9 @@ import { AppContext } from '../Context';
 export default function Navbar() {
   const navigate = useNavigate();
   const allContext = useContext(AppContext);
-  const { currentUser, isEducator, setCurrentUser, setIsEducator,setIsStudent } = allContext;
+  const {
+    currentUser, setCurrentUser, isEducator, setIsEducator, isStudent, setIsStudent,
+  } = allContext;
 
   function handleLogOut() {
     localStorage.clear();
@@ -23,6 +25,25 @@ export default function Navbar() {
     <div className="Navbar">
       <Link to="/">MathFacts Home</Link>
       {isEducator ? <Link to="/educator/home">Dashboard</Link> : ''}
+      {isStudent ? <Link to="/student/home">Dashboard</Link> : ''}
+      {isEducator ? (
+        <p>
+          Logged in as
+          {' '}
+          {currentUser.educator_first_name}
+          {' '}
+          { currentUser.educator_last_name}
+        </p>
+      ) : ''}
+      {isStudent ? (
+        <p>
+          Logged in as
+          {' '}
+          {currentUser.student_first_name}
+          {' '}
+          { currentUser.student_last_name}
+        </p>
+      ) : ''}
       {currentUser != null ? (
         <button
           type="button"
