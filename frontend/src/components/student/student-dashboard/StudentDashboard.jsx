0a613b1b5e2174d3842d617c2bc85cd781b90168
Tiela Black-Law <tiela.rose@gmail.com>
2023-06-24 13:41:15 -0700

    Update StudentDashboard to display more detailed info about student's current problem set (name, type, level)

--- frontend/src/components/student/student-dashboard/StudentDashboard.jsx
+++ frontend/src/components/student/student-dashboard/StudentDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../../Context';
 
@@ -6,6 +6,15 @@ export default function StudentDashboard() {
   const navigate = useNavigate();
   const allContext = useContext(AppContext);
   const { currentUser } = allContext;
+  const [problemSetInfo, setProblemSetInfo] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/problem-set-info/${currentUser.current_problem_set}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProblemSetInfo(data);
+      });
+  }, []);
 
   function handleLetsBeginClick() {
     navigate('/student/assessment');
@@ -23,9 +32,15 @@ export default function StudentDashboard() {
       </h2>
 
       <p>
-        You are working on Goal
+        You are working on
         {' '}
-        {currentUser.current_problem_set}
+        { problemSetInfo.problem_set_type}
+        , Goal
+        {' '}
+        {problemSetInfo.problem_set_level}
+        :
+        {' '}
+        { problemSetInfo.problem_set_name}
       </p>
 
       <p>Ready to practice?</p>
