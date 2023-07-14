19ec379e9ba13cfbef0e20f8ecf9f16dbba9eff8
Tiela Black-Law <tiela.rose@gmail.com>
2023-07-13 20:41:22 -0700

    Refactor handleStrategyButtonClick function to account for baselineTime

--- frontend/src/components/student/student-assessment/AssessmentContainer.jsx
+++ frontend/src/components/student/student-assessment/AssessmentContainer.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import AssessmentQuestion from './AssessmentQuestion';
 import NumberButtonsContainer from './NumberButtonsContainer';
 import './AssessmentContainer.css';
 import StrategyButtonsContainer from './StrategyButtonsContainer';
+import { AppContext } from '../../Context';
 
-export default function AssessmentQuestions({ problemSetQuestions, setHasAnsweredAllQuestions }) {
+const allStudentResponses = [];
+
+export default function AssessmentQuestions(
+  { problemSetQuestions, setHasAnsweredAllQuestions, baselineTime },
+) {
+  const allContext = useContext(AppContext);
+  const { currentUser } = allContext;
   const [currentQuestionNum, setCurrentQuestionNum] = useState(0);
   const [inputAnswer, setInputAnswer] = useState('?');
   const [answerTime, setAnswerTime] = useState(0);
@@ -22,6 +29,41 @@ export default function AssessmentQuestions({ problemSetQuestions, setHasAnswere
     setDisplayNumberButtons(false);
   });
 
+  const handleStrategyButtonClick = useCallback((evt) => {
+    const buttonValue = (evt.target.parentNode.value || evt.target.value);
+    const isFluentStrategy = buttonValue === 'fluent_strategy';
+    const question = problemSetQuestions[currentQuestionNum];
+    const isCorrect = parseInt(inputAnswer, 10) === question.answer_as_int;
+    const isFluentTime = answerTime <= (baselineTime + 4);
+
+    const problemSetQuestionAnswer = {
+      problem_set_question_id: question.question_id,
+      student_answer: parseInt(inputAnswer, 10),
+      is_correct: isCorrect,
+      time_to_answer: answerTime,
+      is_fluent: isFluentStrategy && isCorrect && isFluentTime,
+      baseline_time: baselineTime,
+    };
+
+    allStudentResponses.push(problemSetQuestionAnswer);
+
+    // if the student reaches the last question, stop and send the data to the server
+    if (currentQuestionNum === (problemSetQuestions.length - 1)) {
+      const formInputs = { student_id: currentUser.student_id, all_answers: allStudentResponses };
+
+      fetch('/api/student/submitanswers', {
+        method: 'POST',
+        body: JSON.stringify(formInputs),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setHasAnsweredAllQuestions(true);
+    } else {
+      setInputAnswer('?');
+      setDisplayNumberButtons(true);
+      setCurrentQuestionNum(() => (currentQuestionNum + 1));
+    }
+  });
+
   return (
     <div className="AssessmentContainer">
       <AssessmentQuestion
@@ -34,14 +76,7 @@ export default function AssessmentQuestions({ problemSetQuestions, setHasAnswere
         />
       ) : (
         <StrategyButtonsContainer
-          problemSetQuestions={problemSetQuestions}
-          currentQuestionNum={currentQuestionNum}
-          setHasAnsweredAllQuestions={setHasAnsweredAllQuestions}
-          setInputAnswer={setInputAnswer}
-          setDisplayNumberButtons={setDisplayNumberButtons}
-          setCurrentQuestionNum={setCurrentQuestionNum}
-          inputAnswer={inputAnswer}
-          answerTime={answerTime}
+          handleStrategyButtonClick={handleStrategyButtonClick}
         />
       )}
     </div>
--- frontend/src/components/student/student-assessment/StrategyButtonsContainer.jsx
+++ frontend/src/components/student/student-assessment/StrategyButtonsContainer.jsx
@@ -5,50 +5,10 @@ import ThinkingIcon from '../../../assets/thinking-icon.png';
 import BrainIcon from '../../../assets/brain-icon.png';
 import LightBulbIcon from '../../../assets/lightbulb-icon.png';
 import QuestionMarkIcon from '../../../assets/question-mark-icon.png';
-import { AppContext } from '../../Context';
-
-const allStudentResponses = [];
 
 export default function StrategyButtonsContainer({
-  problemSetQuestions, currentQuestionNum, setHasAnsweredAllQuestions,
-  setInputAnswer, setDisplayNumberButtons, setCurrentQuestionNum,
-  inputAnswer, answerTime,
+  handleStrategyButtonClick
 }) {
-  const allContext = useContext(AppContext);
-  const { currentUser } = allContext;
-
-  function handleStrategyButtonClick(evt) {
-    const buttonValue = (evt.target.parentNode.value || evt.target.value);
-    const isFluentStrategy = buttonValue === 'fluent_strategy';
-    const question = problemSetQuestions[currentQuestionNum];
-    const isCorrect = parseInt(inputAnswer, 10) === question.answer_as_int;
-
-    const problemSetQuestionAnswer = {
-      problem_set_question_id: question.question_id,
-      student_answer: parseInt(inputAnswer, 10),
-      is_correct: isCorrect,
-      time_to_answer: answerTime,
-      is_fluent: isFluentStrategy && isCorrect,
-    };
-
-    allStudentResponses.push(problemSetQuestionAnswer);
-
-    // if the student reaches the last question, stop and send the data to the server
-    if (currentQuestionNum === (problemSetQuestions.length - 1)) {
-      const formInputs = { student_id: currentUser.student_id, all_answers: allStudentResponses };
-
-      fetch('/api/student/submitanswers', {
-        method: 'POST',
-        body: JSON.stringify(formInputs),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      setHasAnsweredAllQuestions(true);
-    } else {
-      setInputAnswer('?');
-      setDisplayNumberButtons(true);
-      setCurrentQuestionNum(() => (currentQuestionNum + 1));
-    }
-  }
 
   const strategyButtonInfo = [
     {
