76f00c18f89de0a33ed85f6cfb0101887069c436
Tiela Black-Law <tiela.rose@gmail.com>
2023-07-18 19:30:45 -0700

    Add route restrictions--if a user tries to visit a restricted route without the correct credentials, they are redirected

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ContextProvider from './Context';
+import React, { useContext } from 'react';
+import {
+  Route, Routes, useLocation,
+} from 'react-router-dom';
+import { AppContext } from './Context';
 
 // Navbar & Home Components
 import Navbar from './navbar/Navbar';
@@ -23,58 +25,76 @@ import StudentDashboard from './student/student-dashboard/StudentDashboard';
 import Assessment from './student/student-assessment/Assessment';
 
 function App() {
+  const { pathname } = useLocation();
+  const allContext = useContext(AppContext);
+  const { isStudent, isEducator } = allContext;
+
+  const educatorRestrictedRoutes = new Set(
+    ['/educator/home', '/educator/new/classroom', '/educator/new/student', '/educator/studentdetails'],
+  );
+
+  const studentRestrictedRoutes = new Set([
+    '/student/home', '/student/assessment',
+  ]);
+
+  if ((educatorRestrictedRoutes.has(pathname)) && !isEducator) {
+    return <Homepage />;
+  }
+
+  if ((studentRestrictedRoutes.has(pathname)) && !isStudent) {
+    return <Homepage />;
+  }
+
   return (
-    <BrowserRouter>
-      <ContextProvider>
-        <Navbar />
-        <div className="main-content">
-          <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route
-              exact
-              path="/educator/login"
-              element={(
-                <EducatorLogin />
+    <>
+      <Navbar />
+      <div className="main-content">
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route
+            exact
+            path="/educator/login"
+            element={(
+              <EducatorLogin />
             )}
-            />
-            <Route
-              exact
-              path="/educator/signup"
-              element={(
-                <EducatorSignup />
+          />
+          <Route
+            exact
+            path="/educator/signup"
+            element={(
+              <EducatorSignup />
             )}
-            />
-            <Route
-              exact
-              path="/educator/home"
-              element={<EducatorDashboard />}
-            />
-            <Route exact path="/educator/new/student" element={<NewStudent />} />
-            <Route
-              exact
-              path="/educator/new/classroom"
-              element={<NewClassroom />}
-            />
-            <Route
-              exact
-              path="/educator/studentdetails"
-              element={<StudentDetails />}
-            />
-            <Route exact path="/student/login" element={<StudentLogin />} />
-            <Route
-              exact
-              path="/student/home"
-              element={<StudentDashboard />}
-            />
-            <Route
-              exact
-              path="/student/assessment"
-              element={<Assessment />}
-            />
-          </Routes>
-        </div>
-      </ContextProvider>
-    </BrowserRouter>
+          />
+          <Route
+            exact
+            path="/educator/home"
+            element={<EducatorDashboard />}
+          />
+          <Route exact path="/educator/new/student" element={<NewStudent />} />
+          <Route
+            exact
+            path="/educator/new/classroom"
+            element={<NewClassroom />}
+          />
+          <Route
+            exact
+            path="/educator/studentdetails"
+            element={<StudentDetails />}
+          />
+          <Route exact path="/student/login" element={<StudentLogin />} />
+          <Route
+            exact
+            path="/student/home"
+            element={<StudentDashboard />}
+          />
+          <Route
+            exact
+            path="/student/assessment"
+            element={<Assessment />}
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/css/bootstrap.css';
+import ContextProvider from './components/Context';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
